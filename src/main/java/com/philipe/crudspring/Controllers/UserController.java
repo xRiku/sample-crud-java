21b727964f77ec0ad2f283317ce4ee9f8f428a53
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2022-12-07 19:08:13 -0300

    Simplest version of api done

--- src/main/java/com/philipe/crudspring/Controllers/UserController.java
+++ src/main/java/com/philipe/crudspring/Controllers/UserController.java
@@ -1,9 +1,17 @@
 package com.philipe.crudspring.Controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.philipe.crudspring.Models.User;
 import com.philipe.crudspring.Repository.UserRepository;
 @RestController
 public class UserController {
@@ -13,7 +21,48 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/")
-    public String listUsers() {
-        return userRepository.findAll().toString();
+    public String index() {
+        return "Hello\nAccess /user to list all users ";
+    }
+
+    @GetMapping("/user")
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/user")
+    public String createUser(@RequestBody User user) {
+        userRepository.save(user);
+        return "User added";
+    }
+
+    @PutMapping("/user/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        User userToUpdate = userRepository.findById(id).get();
+        System.out.println(user.getFirstName());
+        if (user.getFirstName() != null) {
+            userToUpdate.setFirstName(user.getFirstName());
+        }
+        if (user.getLastName() != null) {
+            userToUpdate.setLastName(user.getLastName());
+        }
+        if (user.getAge() != 0) {
+            userToUpdate.setAge(user.getAge());
+        }
+        userRepository.save(userToUpdate);
+
+        return "User updated";
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable long id) {
+        userRepository.deleteById(id);
+        return "User deleted";
+    }
+
+    @DeleteMapping("/user/all")
+    public String deleteAll() {
+        userRepository.deleteAll();
+        return "All users deleted";
     }
 }
--- src/main/java/com/philipe/crudspring/Models/User.java
+++ src/main/java/com/philipe/crudspring/Models/User.java
@@ -21,4 +21,34 @@ public class User {
 
     @Column
     private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
 }
